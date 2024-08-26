f517d3e20d1a9debf5c6681aaff61d607b19215f
baojunGit <18006996859@163.com>
2024-08-26 18:31:24 +0800

    feature: 增加回到顶部功能

--- src/Layout/index.tsx
+++ src/Layout/index.tsx
@@ -18,20 +18,17 @@ const Layout = () => {
 						<AppTabs />
 					</div>
 					{/* 页面内容区 */}
-					<AppPage  />
+					<AppPage />
 				</main>
 			</div>
 			{/* 是要在app-page里识别滚动 */}
-			{/* <BackTop
-        target={() => document.getElementById('app-page')}
-        visibilityHeight={100}
-      /> */}
 			<MyBackTop
 				// querySelector() 方法可以用于返回文档中匹配指定 CSS 选择器的一个元素，
 				// 如果需要返回所有的元素，就需要使用querySelectorAll() 方法
 				target={() => document.querySelector('#app-page')}
 				visibilityHeight={100}
 			/>
+			<MyBackTop />
 		</div>
 	);
 };
